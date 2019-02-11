4faa15cdf52398d9f5c5d75c4387a6550a509db9
Armand <armand.briquet@gmail.com>
2019-02-12 00:04:47 +0100

    Shot movement animation + crosshair

--- Projet Unity MTI/Assets/Scripts/gun.cs
+++ Projet Unity MTI/Assets/Scripts/gun.cs
@@ -5,16 +5,24 @@ public class gun : MonoBehaviour
     public float damage = 10f;
     public float range = 100f;
 
+    public Animator anim;
     public Camera  fpsCam;
     public ParticleSystem muzzleFLash;
 
     public GameObject impactEffect;
 
     // Update is called once per frame
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
+            print("yes");
+            anim.Play("shot");
             Shoot();
         }
     }
@@ -22,12 +30,12 @@ public class gun : MonoBehaviour
 
     void Shoot()
     {
+
         muzzleFLash.Play();
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position,fpsCam.transform.forward,out hit,range))
         {
-            Debug.Log(hit.transform.name);
-
+           
             target target  = hit.transform.GetComponent<target>();
             if (target != null)
             {
@@ -36,8 +44,8 @@ public class gun : MonoBehaviour
 
             GameObject impactGo =  Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(impactGo, 2f);
-            
-            
+           
+
         }
     }
 }
