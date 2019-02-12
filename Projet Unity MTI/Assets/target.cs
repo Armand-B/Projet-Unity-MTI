b2f9b7549fa1b9faf0a1aa42bf6fb64ac2e241db
Armand <armand.briquet@gmail.com>
2019-02-12 13:38:50 +0100

    Death sound

--- Projet Unity MTI/Assets/target.cs
+++ Projet Unity MTI/Assets/target.cs
@@ -3,21 +3,25 @@ using UnityEngine;
 
 public class target : MonoBehaviour
 {
+    public AudioSource cri;
     public float health = 50f;
     public GameObject deathEffect;
-
+    int die = 0;
     public void TakeDamage(float amount)
     {
         health -= amount;
-        if (health <= 0f)
+        if (health <= 0f && die == 0)
         {
+            cri.Play();
             Die();
         }
     }
 
     void Die()
     {
-        Destroy(gameObject);
+        die = 1;
+        
+        Destroy(gameObject,0.552f);
         GameObject explosion =  Instantiate(deathEffect, transform.position ,transform.rotation);
         Destroy(explosion, 2f);
     }
