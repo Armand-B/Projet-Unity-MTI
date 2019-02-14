316cf504e2b4e663fd24a29aa56c05d1067a391d
Lieema <axel.vigny@epita.fr>
2019-02-14 22:26:41 +0100

    Enemy correction (no more slippery) and solare panel destruction by enemy

---
+++ Projet Unity MTI/Assets/Scripts/DamageHandler.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHandler : MonoBehaviour
+{
+    public int maxHealth = 30;
+    public int secondsInvicibility;
+    public AudioSource deathSound;
+    public GameObject deathEffect;
+
+    private int currentHealth;
+    private bool gotHit = false;
+    private float timeLeft = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            print("dead");
+            Destroy(this.gameObject, 0.552f);
+            if (deathSound != null)
+            {
+                deathSound.Play();
+            }
+            if (deathEffect != null)
+            {
+                GameObject explosion = Instantiate(deathEffect, transform.position, transform.rotation);
+                Destroy(explosion, 2f);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            print(currentHealth);
+            if (!gotHit)
+            {
+                gotHit = true;
+                timeLeft = secondsInvicibility;
+                currentHealth -= collision.gameObject.GetComponent<EnemyMovment>().damage;
+                if (currentHealth < 0)
+                    currentHealth = 0;
+            }
+            else
+            {
+                if (timeLeft <= 0)
+                {
+                    gotHit = false;
+                }
+            }
+        }
+    }
+}
--- Projet Unity MTI/Assets/Scripts/EnemyMovment.cs
+++ Projet Unity MTI/Assets/Scripts/EnemyMovment.cs
@@ -28,12 +28,32 @@ public class EnemyMovment : MonoBehaviour
 
     private void SetDestination()
     {
-        _destination = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        List<GameObject> l = new List<GameObject>(GameObject.FindGameObjectsWithTag("SolarPanel"));
+        l.Add(GameObject.FindWithTag("Player"));
+        _destination = GetClosestEnemy(l.ToArray());
+    
         if (_destination != null)
         {
             Vector3 targetVector = _destination.transform.position;
             _navMeshAgent.SetDestination(targetVector);
         }
+    }
+
+    private Transform GetClosestEnemy(GameObject[] enemies)
+    {
+        Transform tMin = null;
+        float minDist = Mathf.Infinity;
+        Vector3 currentPos = transform.position;
+        foreach (GameObject t in enemies)
+        {
+            float dist = Vector3.Distance(t.GetComponent<Transform>().position, currentPos);
+            if (dist < minDist)
+            {
+                tMin = t.GetComponent<Transform>();
+                minDist = dist;
+            }
+        }
+        return tMin;
     }
 
     // Update is called once per frame
