5509151388879e31a7a31ad28878bc28c7db4f04
Lieema <axel.vigny@epita.fr>
2019-02-14 19:00:41 +0100

    Add energy handle

--- Projet Unity MTI/Assets/Scripts/HealthEnergyHandle.cs
+++ Projet Unity MTI/Assets/Scripts/HealthEnergyHandle.cs
@@ -11,12 +11,17 @@ public class HealthEnergyHandle : MonoBehaviour
     public Text healthText;
     public Slider energyBar;
     public Text energyText;
+    public Text timeOfDay;
     public int secondsInvicibility;
+    public int energyGivenPerTick;
+    public int timeBetweenEnergy;
 
     private int currentHealth;
     private int currentEnergy;
     private bool gotHit = false;
     private float timeLeft = 0;
+    private float whenEnergy = 0;
+    private int nbSolarPanels;
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +36,8 @@ public class HealthEnergyHandle : MonoBehaviour
         currentEnergy = 0;
         healthText.text = "Health : " + healthBar.value;
         energyText.text = "Energy : " + energyBar.value;
+        whenEnergy = 0;
+        nbSolarPanels = GameObject.FindGameObjectsWithTag("SolarPanel").Length;
     }
 
     // Update is called once per frame
@@ -41,6 +48,28 @@ public class HealthEnergyHandle : MonoBehaviour
         healthText.text = "Health : " + healthBar.value;
         energyText.text = "Energy : " + energyBar.value;
         timeLeft -= Time.deltaTime;
+        nbSolarPanels = GameObject.FindGameObjectsWithTag("SolarPanel").Length;
+        UpdateEnergy();
+    }
+
+    void UpdateEnergy()
+    {
+        if (isDay())
+        {
+            
+            if (whenEnergy <= 0)
+            {
+                for (int i = 0; i < nbSolarPanels; ++i)
+                {
+                    currentEnergy += energyGivenPerTick;
+                }
+                whenEnergy = timeBetweenEnergy;
+            }
+            else
+            {
+                whenEnergy -= Time.deltaTime;
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -52,6 +81,8 @@ public class HealthEnergyHandle : MonoBehaviour
                 gotHit = true;
                 timeLeft = secondsInvicibility;
                 currentHealth -= collision.gameObject.GetComponent<EnemyMovment>().damage;
+                if (currentHealth < 0)
+                    currentHealth = 0;
             }
             else
             {
@@ -61,5 +92,14 @@ public class HealthEnergyHandle : MonoBehaviour
                 }
             }
         }
+    }
+
+    private bool isDay()
+    {
+        if (timeOfDay.text == "Morning" ||
+            timeOfDay.text == "Mid Noon" ||
+            timeOfDay.text == "Evening")
+            return true;
+        return false;
     }
 }
