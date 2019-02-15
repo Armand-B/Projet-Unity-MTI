c8bf8ec23937074952ce20ad7c140c038c2c3219
Armand <armand.briquet@gmail.com>
2019-02-15 14:29:33 +0100

    map generation fixed

--- Projet Unity MTI/Assets/Scripts/BuildCity.cs
+++ Projet Unity MTI/Assets/Scripts/BuildCity.cs
@@ -9,7 +9,7 @@ public class BuildCity : MonoBehaviour
     public GameObject[] buildings;
     public int mapWith = 20;
     public int mapHeight = 20;
-    public float buildingFootPrint = 1.5f;
+    public float buildingFootPrint;
     int[] angles = new[] { 0, 90, 180, 270 };
 
     // Start is called before the first frame update
@@ -20,12 +20,9 @@ public class BuildCity : MonoBehaviour
             for (int w = 0; w < mapWith; w++)
             {
                 System.Random r = new System.Random();
-               
-
-                    Vector3 pos = new Vector3(w * buildingFootPrint, 0, h * buildingFootPrint);
-                    int n = UnityEngine.Random.Range(0, buildings.Length);
-                    print("rotation : " + angles[r.Next(angles.Length)]);          
-                    Instantiate(buildings[n], pos, Quaternion.Euler(0, angles[r.Next(angles.Length)], 0));
+                Vector3 pos = new Vector3(w * buildingFootPrint, 0, h * buildingFootPrint);
+                int n = UnityEngine.Random.Range(0, buildings.Length);       
+                Instantiate(buildings[n], pos, /*Quaternion.Euler(0, angles[r.Next(angles.Length)], 0)*/ Quaternion.identity);
                
 
                 
